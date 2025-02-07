cbed9093980f8d72c3438632ab95788140cbdb71
Mattia-FR <mattia.bouvier@protonmail.com>
2025-02-07 14:59:34 +0100

    Update of the paths.

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ app.use(cors());
 // Servir les fichiers statiques depuis le dossier "public"
 app.use("/static", express.static("assets"));
 
+// Route racine
 app.get("/", (req, res) => {
 	res.json("je suis dans le /");
 });
 
+// Route pour récupérer TOUS les emplois
 app.get("/emplois", (req, res) => {
 	res.json(dataEmplois);
+});
+
+// **Route pour récupérer un emploi spécifique par ID**
+app.get("/emplois/:id", (req, res) => {
+	const id = parseInt(req.params.id, 10); // Convertir l'ID en nombre
+
+	// Recherche de l'emploi dans les données
+	const emploi = dataEmplois.find((job) => job.id === id);
+
+	if (emploi) {
+		res.json(emploi); // Retourner les détails de l'emploi trouvé
+	} else {
+		res.status(404).send("Emploi non trouvé"); // Retourner une erreur si l'emploi n'existe pas
+	}
 });
 
 app.listen(4242, () => console.log("http://localhost:4242"));
